e5cfbf31919bb4113fbbad61918f6680a7133a23
David Papp <pappdav@gmail.com>
2024-10-30 17:04:52 +0100

    chore: Test improvements

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,27 +2,31 @@ import { unstable_dev } from "wrangler";
 import type { UnstableDevWorker } from "wrangler";
 import { describe, expect, it, beforeAll, afterAll } from "vitest";
 
-describe("Chat Endpoint", () => {
-	let worker: UnstableDevWorker;
+describe('Chat Endpoint', () => {
+  let worker: UnstableDevWorker;
 
-	beforeAll(async () => {
-		worker = await unstable_dev("src/index.ts", {
-			experimental: { disableExperimentalWarning: true },
-		});
-	});
+  beforeAll(async () => {
+    worker = await unstable_dev('src/index.ts', {
+      experimental: { disableExperimentalWarning: true },
+    });
+  });
 
-	afterAll(async () => {
-		await worker.stop();
-	});
+  afterAll(async () => {
+    await worker.stop();
+  });
 
-	it("should return non-streaming response", async () => {
-		const resp = await worker.fetch("/openai/v1/chat/completions", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ stream: false }),
-		});
+  it('should return non-streaming response', async () => {
+    const resp = await worker.fetch('/openai/v1/chat/completions', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        stream: false,
+        model: 'gpt-4o',
+        messages: [{ role: 'user', content: 'Hello!' }],
+      }),
+    });
 
-		if (resp) {
+    if (resp) {
       const json = (await resp.json()) as {
         choices: { message: { content: string } }[];
       };
@@ -31,31 +35,60 @@ describe("Chat Endpoint", () => {
         "As an AI, I don't have personal beliefs or feelings."
       );
     }
-	}, 15000);
+  }, 15000);
 
-	it("should return streaming response", async () => {
-		const resp = await worker.fetch("/openai/v1/chat/completions", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ stream: true, streamingDelay: 100 }),
-		});
+  it('should return streaming response', async () => {
+    const resp = await worker.fetch('/openai/v1/chat/completions', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        stream: true,
+        streamingDelay: 100,
+        model: 'gpt-4o',
+        messages: [{ role: 'user', content: 'Hello!' }],
+      }),
+    });
 
-		if (resp) {
-			const reader = resp.body?.getReader();
-			const decoder = new TextDecoder();
-			let done = false;
-			let fullText = "";
+    if (resp) {
+      const reader = resp.body?.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+      let fullText = '';
 
-			while (!done) {
-				const { value, done: streamDone } = await reader?.read()!;
-				done = streamDone;
-				if (value) {
-					fullText += decoder.decode(value, { stream: true });
-					// console.log("Received chunk:", decoder.decode(value, { stream: true }));
-				}
-			}
+      while (!done) {
+        const { value, done: streamDone } = await reader?.read()!;
+        done = streamDone;
+        if (value) {
+          fullText += decoder.decode(value, { stream: true });
+          // console.log("Received chunk:", decoder.decode(value, { stream: true }));
+        }
+      }
 
-			expect(fullText).toContain("data: [DONE]");
-		}
-	}, 30000);
+      expect(fullText).toContain('data: [DONE]');
+    }
+  }, 30000);
+
+  describe('Image Generation Endpoint', () => {
+    it('should return an image', async () => {
+      const resp = await worker.fetch('/openai/v1/images/generations', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ prompt: 'A futuristic cityscape' }),
+      });
+
+      if (resp) {
+        const contentType = resp.headers.get('Content-Type');
+        expect(contentType?.startsWith('application/json')).toBe(true);
+
+        const json = (await resp.json()) as {
+          data: { url: string }[];
+        };
+        console.log(json);
+
+        expect(json).toHaveProperty('data');
+        expect(json.data.length).toBeGreaterThan(0);
+      }
+    }, 15000);
+  });
 });
+
--- src/utils/ratelimit.ts
+++ src/utils/ratelimit.ts
@@ -1,13 +1,13 @@
 import { Env } from '..';
 
 export async function ratelimit(request: Request, env: Env, ctx: ExecutionContext) {
-        if (env.RATELIMITING_ENABLED) {
+        if (env.RATELIMITING_ENABLED && env.ENV === 'production') {
         const ipAddress = request.headers.get('cf-connecting-ip') || '';
         const { success } = await env.RATELIMITER.limit({ key: ipAddress });
         const { pathname } = new URL(request.url);
 
         if (!success) {
           return new Response(`429 Failure – rate limit exceeded for ${pathname}`, { status: 429 });
-        } 
+        }
   }
 }
